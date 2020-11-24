9ae485466f587ec8afcd354ca16dbaec3dfb0db3
SyzonVOV <sizonenko89@gmail.com>
2020-11-24 14:07:05 +0200

    Beauty changes

--- mongo.js
+++ mongo.js
@@ -1,34 +1,40 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 
 const argvLength = process.argv.length;
 
 if (argvLength < 3) {
-  console.log('Please provide the password as an argument: node mongo.js <password>')
-  process.exit(1)
+  console.log(
+    'Please provide the password as an argument: node mongo.js <password>',
+  );
+  process.exit(1);
 }
 
-const password = process.argv[2]
+const password = process.argv[2];
 
-const url =
-  `mongodb+srv://user_ad:${password}@cluster0.plmmz.mongodb.net/phonebook-app?retryWrites=true&w=majority`
+const url = `mongodb+srv://user_ad:${password}@cluster0.plmmz.mongodb.net/phonebook-app?retryWrites=true&w=majority`;
 
-mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
+mongoose.connect(url, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+  useCreateIndex: true,
+});
 
 const personSchema = new mongoose.Schema({
   name: String,
   number: String,
-})
+});
 
-const Person = mongoose.model('Person', personSchema)
+const Person = mongoose.model('Person', personSchema);
 
-if (argvLength == 3) {
+if (argvLength === 3) {
   Person.find({}).then(result => {
     console.log('phonebook:');
     result.forEach(person => {
-      console.log(person.name+' '+person.number)
-    })
-    mongoose.connection.close()
-  })
+      console.log(person.name + ' ' + person.number);
+    });
+    mongoose.connection.close();
+  });
 }
 
 if (argvLength > 3) {
@@ -41,8 +47,6 @@ if (argvLength > 3) {
   person.save().then(result => {
     console.log(result);
     console.log(`added ${name} number ${number} to phonebook`);
-    mongoose.connection.close()
-  })
+    mongoose.connection.close();
+  });
 }
-
-
